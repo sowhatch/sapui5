4e315ee060a1e9a60a8e587be00778593e4bcbcb
sowhatch <cogml4574@gmail.com>
2023-09-12 18:31:55 +0900

    sap.ui.table.Table Exercise-1

---
+++ test.ui.table/webapp/controller/View1.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("sync.c18.test.ui.table.controller.View1", {
+            onInit: function () {
+
+            }
+        });
+    });
---
+++ test.ui.table/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"syncc18/test.ui.table/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
